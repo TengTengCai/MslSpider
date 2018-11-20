c57c9569a13549e1efec11feb99bb2fbb752c314
tianjun <1021766585@qq.com>
2018-11-20 09:01:48 +0800

    修改代码格式

--- MslSpider/spiders/islamqa.py
+++ MslSpider/spiders/islamqa.py
@@ -18,7 +18,7 @@ class IslamqaSpider(CrawlSpider):
         Rule(LinkExtractor(allow=r'https://islamqa.info/ar/answers/\d+/.*'), callback='parse_ar', follow=True),
         Rule(LinkExtractor(allow=r'https://islamqa.info/zh/answers/\d+/.*'), callback='parse_zh', follow=True),
         Rule(LinkExtractor(allow=r'https://islamqa.info/en/answers/\d+/.*'), callback='parse_en', follow=True),
-        Rule(LinkExtractor(allow=r'https://islamqa.info/bn/answers/\d+/.*'), callback='parse_en', follow=True),
+        Rule(LinkExtractor(allow=r'https://islamqa.info/bn/answers/\d+/.*'), callback='parse_bn', follow=True),
         Rule(LinkExtractor(allow=r'https://islamqa.info/[a-z]{2}/categories/topics/\d+/.*'), callback='parse_new_page',
              follow=True),
     )
@@ -28,6 +28,7 @@ class IslamqaSpider(CrawlSpider):
             'ar',
             'zh',
             'en',
+            'bn',
         ]
         for k in lang_list:
             for i in range(1, 300):
@@ -109,7 +110,7 @@ class IslamqaSpider(CrawlSpider):
         answer_list = response.xpath(
             '/html/body/section/div[1]/div/section/div/div[2]/section/section/div/p/text()').extract()
         a_list = [a.strip("\n\r").replace("r\n\\", " ").replace("\r\n", " ").replace("\n", " ").replace("\r", " ")
-                      .replace("   ", '') for a in answer_list]
+                  .replace("   ", '') for a in answer_list]
         # answer = ' '.join(answer_list).replace("r\n\\", " ").replace("\r\n", " ").replace("\n", " ") \
         #     .replace("\r", " ")
         answer = ' '.join(a_list)
@@ -151,7 +152,7 @@ class IslamqaSpider(CrawlSpider):
         answer_list = response.xpath(
             '/html/body/section/div[1]/div/section/div/div[2]/section/section/div/p/text()').extract()
         a_list = [a.strip("\n\r").replace("r\n\\", " ").replace("\r\n", " ").replace("\n", " ").replace("\r", " ")
-                      .replace("   ", '') for a in answer_list]
+                  .replace("   ", '') for a in answer_list]
         # answer = ' '.join(answer_list).replace("r\n\\", " ").replace("\r\n", " ").replace("\n", " ") \
         #     .replace("\r", " ")
         answer = ' '.join(a_list)
@@ -162,5 +163,47 @@ class IslamqaSpider(CrawlSpider):
         i['url_mark'] = m.hexdigest()
         i['r_type'] = "islamqa"
         i['lang'] = "en"
+        del m
+        return i
+
+    def parse_bn(self, response):
+        m = hashlib.md5()
+        m.update(str(response.url).encode('utf-8'))
+        i = MslspiderItem()
+        title = response.xpath('//h1[@itemprop="name"]/text()').extract_first()
+        if title:
+            i['title'] = response.xpath('//h1[@itemprop="name"]/text()').extract_first().replace('\n', '') \
+                .replace('   ', '')
+        else:
+            return None
+        nav = response.xpath('//nav[@aria-label="breadcrumbs"]')
+        if len(nav) > 0:
+            i['tag'] = str(response.xpath('//span[@itemprop="name"]/text()').extract()[-1]).replace('\n', '') \
+                .replace('  ', '')
+            i['categories'] = response.xpath('//span[@itemprop="name"]/text()').extract()[1].replace('\n', '') \
+                .replace('  ', '')
+        else:
+            i['tag'] = ''
+            i['categories'] = ''
+        question_list = response.xpath('/html/body/section/div[1]/div/section/div/section[2]/div//text()').extract()
+        q_list = [q.strip("\n\r").replace("r\n\\", " ").replace("\r\n", " ").replace("\n", " ").replace("\r", " ")
+                      .replace("   ", '') for q in question_list]
+        # i['question'] = ' '.join(question_list).replace("r\n\\", " ").replace("\r\n", " ").replace("\n", " ") \
+        #     .replace("\r", " ")
+        i['question'] = ' '.join(q_list)
+        answer_list = response.xpath(
+            '/html/body/section/div[1]/div/section/div/div[2]/section/section/div/p/text()').extract()
+        a_list = [a.strip("\n\r").replace("r\n\\", " ").replace("\r\n", " ").replace("\n", " ").replace("\r", " ")
+                  .replace("   ", '') for a in answer_list]
+        # answer = ' '.join(answer_list).replace("r\n\\", " ").replace("\r\n", " ").replace("\n", " ") \
+        #     .replace("\r", " ")
+        answer = ' '.join(a_list)
+        i['answer'] = answer
+        qa_id = response.xpath('/html/body/section/div[1]/div/section/div/div[1]/div/div[1]/div[1]/p/text()') \
+            .extract()[1].split()[0]
+        i['qa_id'] = qa_id
+        i['url_mark'] = m.hexdigest()
+        i['r_type'] = "islamqa"
+        i['lang'] = "bn"
         del m
         return i
