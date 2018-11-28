817f3bf9f8fe7d48fce87cc6c4fa50e4ea4d2083
tianjun <1021766585@qq.com>
2018-11-28 17:05:19 +0800

    修改爬取规则

--- MslSpider/spiders/binbaz.py
+++ MslSpider/spiders/binbaz.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 import hashlib
 import re
+from logging import log, DEBUG
 from urllib.parse import urljoin
 
 from scrapy import Request
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 
-from MslSpider.items import MslspiderItem
+from MslSpider.items import MslspiderBinbazItem
 from MslSpider.settings import binbaz_settings
 
 
@@ -21,8 +22,8 @@ class BinbazSpider(CrawlSpider):
         # Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
         Rule(LinkExtractor(allow=r'https://binbaz.org.sa/fatwas/\d+/.*'), callback='parse_fatwas', follow=True),
         # Rule(LinkExtractor(allow=r'/fatwas/\d+/.*?'), callback='parse_fatwas', follow=True),
-        Rule(LinkExtractor(allow=r'https://binbaz.org.sa/categories/fiqhi/\d+[/fatwa]?[?page=\d+.*]?'), callback='parse_page',
-             follow=True),
+        Rule(LinkExtractor(allow=r'https://binbaz.org.sa/categories/.*?/\d+[/fatwa]?[?page=\d+.*]?'),
+             callback='parse_page', follow=True),
         # Rule(LinkExtractor(allow=r'/categories/fiqhi/\d+?page=\d+'), callback='parse_page',
         #      follow=True),
         # Rule(LinkExtractor(allow=r'https://binbaz.org.sa/audios/\d+/.*?'), callback='parse_audios', follow=True),
@@ -49,19 +50,29 @@ class BinbazSpider(CrawlSpider):
         """
         m = hashlib.md5()
         m.update(str(response.url).encode('utf-8'))
-        i = MslspiderItem()
+        i = MslspiderBinbazItem()
         title = response.xpath('//h1[@class="article-title article-title--primary"]/text()').extract_first(default='')
         i['title'] = ''.join(title).replace('\n', '').replace('n\\', '').replace('   ', '')
-        i['tag'] = response.xpath('//div[@class="categories"]/*[4]/text()').extract_first(default='')
-        i['categories'] = response.xpath('//div[@class="categories"]/*[2]/text()').extract_first(default='')
+        objective_href = response.xpath(
+            '//div[@class="utility__internal-border-top utility__internal-border-top--no-padding"]'
+            '//i[@class="categories__icon fa fa-tag"][1]/following-sibling::a[1]/@href').extract_first(default='')
+        legal_href = response.xpath(
+            '//div[@class="utility__internal-border-top utility__internal-border-top--no-padding"]'
+            '//i[@class="categories__icon fa fa-sitemap"][1]/following-sibling::a[1]/@href').extract_first(default='')
+        log(DEBUG, (objective_href, legal_href))
+        objective_m = re.search('https://binbaz.org.sa/categories/objective/(\d+)', objective_href)
+        i['objective'] = '' if objective_m is None else objective_m.group(1)
+        legal_m = re.search('https://binbaz.org.sa/categories/fiqhi/(\d+)', legal_href)
+        i['legal'] = '' if legal_m is None else legal_m.group(1)
+
         question_list = response.xpath(
             '//p[@itemprop="articleBody"][1]/preceding-sibling::p//text()|'
             '//p[@itemprop="articleBody"][1]/preceding-sibling::h2//text()').extract()
-        q_list = [str(q).replace("\r\n", " ").replace("\n", " ").replace("\r", " ").replace("   ", '')
+        q_list = [str(q).replace("\r\n", " ").replace("\n", " ").replace("\r", " ").replace("   ", '').strip()
                   for q in question_list]
         i['question'] = '\r\n'.join(q_list).replace('\r\n\r\n', '')
         answer_list = response.xpath('//p[@itemprop="articleBody"][1]/following-sibling::*//text()').extract()
-        a_list = [str(a).replace("\r\n", " ").replace("\n", " ").replace("\r", " ").replace("   ", '')
+        a_list = [str(a).replace("\r\n", " ").replace("\n", " ").replace("\r", " ").replace("   ", '').strip()
                   for a in answer_list]
         i['answer'] = '\r\n'.join(a_list).replace('\r\n\r\n', '')
         i['qa_id'] = re.search('https://binbaz.org.sa/fatwas/(\d+)/.*?', response.url).group(1)
